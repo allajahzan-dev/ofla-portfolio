2b941294eab9ae3bffcb614ccdd464089fce542b
Ahsan allaj pk <ahsanallajpk22@gmail.com>
2025-08-03 13:53:45 +0530

    fix: accessing params with async await

--- src/app/products/[item]/components/sections/MainSection.tsx
+++ src/app/products/[item]/components/sections/MainSection.tsx
@@ -2,15 +2,16 @@ import ContactSection from "@/components/common/section/ContactSection";
 
 // Interface for Props
 interface Props {
-    params: { item: string };
+    params: Promise<{ item: string }>;
 }
 
 // Main section
-export default function MainSection({ params }: Props) {
+export default async function MainSection({ params }: Props) {
+    const { item } = await params;
     return (
         <main>
             <section className="min-h-screen flex flex-col items-center justify-center">
-                <h1 className="text-8xl font-bold">{params.item.toUpperCase()}</h1>
+                <h1 className="text-8xl font-bold">{item.toUpperCase()}</h1>
             </section>
             <ContactSection/>
         </main>
