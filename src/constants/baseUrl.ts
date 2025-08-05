9c2c2d22c64342e819b03fdd67a1da6403d1bd19
Ahsan allaj pk <ahsanallajpk22@gmail.com>
2025-08-05 17:59:24 +0530

    patch: corrected the base_url export-2

--- src/constants/baseUrl.ts
+++ src/constants/baseUrl.ts
@@ -1,10 +1,6 @@
-const baseUrl = () => {
-    let BASE_URL: string;
+const BASE_URL: string =
     process.env.NODE_ENV === "development"
-        ? (BASE_URL = "http://localhost:3000")
-        : (BASE_URL = "https://ofla-portfolio.vercel.app");
+        ? "http://localhost:3000"
+        : "https://ofla-portfolio.vercel.app";
 
-    return BASE_URL;
-};
-
-export default baseUrl();
+export default BASE_URL;
